797a7eeedc044f243f08c712550b8ec0e711af9e
smtamm18 <36050175+smtamm18@users.noreply.github.com>
2018-02-12 13:32:06 -0500

    Add files via upload

--- team5.py
+++ team5.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'raspberry' # Only 10 chars displayed.
+strategy_name = 'mystery'
+strategy_description = 'I am always going to collude, except when I am betrayed.'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -53,7 +53,7 @@ if __name__ == '__main__':
               my_score=0,
               their_score=0,
               result='b'):
-         print 'Test passed'
+         print ('Test passed')
      # Test 2: Continue betraying if they collude despite being betrayed.
     test_move(my_history='bbb',
               their_history='ccc', 
